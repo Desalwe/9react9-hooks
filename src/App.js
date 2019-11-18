4ce908807e0d2658193f9f73ae5520e5a5f0d8a1
Alex Lawry-White <designalexweb@gmail.com>
2019-11-18 18:10:03 +0000

    upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [startingValue, setStartingValue] = useState();
+
+  function handleValueChange(e) {
+    e.preventDefault();
+    setStartingValue(e.target.value);
+  }
+
+  function onButtonClick() {
+    setCount(startingValue);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <br />
+      <input
+        placeholder="Enter a starting Value:"
+        value={startingValue}
+        onChange={handleValueChange}
+        onSubmit={onButtonClick}
+        type="Number"
+      />
+      <br />
+      <button
+        onClick={onButtonClick}
+      >
+        Submit
+      </button>
+      <br />
+      <br />
+      <button onClick={() => setCount(count + 1)}>
+        +1
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        -1
+      </button>
+      <h1>{count}</h1>
     </div>
   );
 }
 
-export default App;
+export default Counter
+
+
+// onChange -> changes display
+// onSumbit -> changes count
